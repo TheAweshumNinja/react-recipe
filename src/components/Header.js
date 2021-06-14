54aa202feb4e8e9b6581ae9a5ca3503b6108ca7f
Samantha <samanthademotta@yahoo.com>
2021-06-14 00:04:21 -0400

    final commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
     return (
         <header>
-            <h1 className="text-center">Reciplease</h1>
+            <h1 className="text-center">Recipe Search</h1>
         </header>
     );
 }
